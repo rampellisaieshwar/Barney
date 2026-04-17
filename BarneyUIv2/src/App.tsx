ec86cd7add3b6c2e67b6f81a8c9857c0d24e81ac
Sai Eshwar Rampelli <saieshwarrampelli@Sais-MacBook-Air.local>
2026-04-17 21:21:53 +0530

    chore: add vercel.json and fix App context usage

--- BarneyUIv2/src/App.tsx
+++ BarneyUIv2/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
   globalStyles();
 
   return (
-    <ThemeProvider>
+    <ThemeProvider.Provider value={{}}>
       <AppContainer>
         <Sidebar />
         <MainContent>
@@ -34,7 +34,7 @@ function App() {
           </MainCanvas>
         </MainContent>
       </AppContainer>
-    </ThemeProvider>
+    </ThemeProvider.Provider>
   );
 }
 
